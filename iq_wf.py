26e9f267b8776755251a3035b407054dde653615
kimjonghyub <ds3dnd@gmail.com>
2024-11-08 17:51:37 +0900

    Update iq_wf.py

--- iq_wf.py
+++ iq_wf.py
@@ -34,31 +34,22 @@ def palette_color(palette, val, vmin0, vmax0):
         return: pygame color tuple
     """
     f = (float(val) - vmin0) / (vmax0 - vmin0)     # btw 0 and 1.0
-    f *= 2
-    f = min(1., max(0., f))
+    f = min(1.0, max(0.0, f * 2))
     if palette == 1:
-        g, b = 0, 0
-        if f < 0.333:
-            r = int(f*255*3)
-        elif f < 0.666:
-            r = 200
-            g = int((f-.333)*255*3)
-        else:
-            r = 200
-            g = 200
-            b = int((f-.666)*255*3)
+        r = int(255 * min(1.0, 3 * f))
+        g = int(255 * min(1.0, 3 * (f - 0.333)))
+        b = int(255 * min(1.0, 3 * (f - 0.666)))
     elif palette == 2:
-        bright = min (1.0, f + 0.15)
-        tpi = 2 * math.pi
-        r = bright * 128 *(1.0 + math.cos(tpi*f))
-        g = bright * 128 *(1.0 + math.cos(tpi*f + tpi/3))
-        b = bright * 128 *(1.0 + math.cos(tpi*f + 2*tpi/3))
+        bright = min(1.0, f + 0.15)
+        tpi_f = 2 * math.pi * f
+        r = bright * 128 * (1.0 + math.cos(tpi_f))
+        g = bright * 128 * (1.0 + math.cos(tpi_f + 2 * math.pi / 3))
+        b = bright * 128 * (1.0 + math.cos(tpi_f + 4 * math.pi / 3))
     else:
-        print ("Invalid palette requested!")
-        sys.exit()
-    return ( max(0,min(255,r)), max(0,min(255,g)), max(0,min(255,b)) )
+        raise ValueError("Invalid palette requested!")
+    return max(0, min(255, int(r))), max(0, min(255, int(g))), max(0, min(255, int(b)))
 
-class Wf(object):
+class Wf:
     """ Make a waterfall '3d' display of spectral power vs frequency & time.
         init: min, max palette parameter, no. of steps between min & max,
         size for each freq,time data plot 'pixel' (a box)
@@ -69,63 +60,52 @@ class Wf(object):
             no. of surfaces = nsteps
         """
         self.opt = opt
-        self.vmin = vmin
-        self.vmin_rst = vmin
-        self.vmax = vmax
-        self.vmax_rst = vmax
-        self.nsteps = nsteps
-        self.pixel_size = pxsz
-        self.firstcalc = True
+        self.vmin, self.vmax = vmin, vmax
+        self.vmin_rst, self.vmax_rst = vmin, vmax
+        self.nsteps, self.pixel_size = nsteps, pxsz
+        self.datasize, self.dx, self.firstcalc = None, None, True
         self.initialize_palette()
         
     def initialize_palette(self):
         """ Set up surfaces for each possible color value in list self.pixels.
         """
-        self.pixels = list()
+        self.pixels = []
+        step_value = (self.vmax - self.vmin) / self.nsteps
         for istep in range(self.nsteps):
-            ps = pg.Surface(self.pixel_size)
-            val = float(istep)*(self.vmax-self.vmin)/self.nsteps + self.vmin
-            color = palette_color(self.opt.waterfall_palette, val, self.vmin, self.vmax)
-            ps.fill( color )
-            self.pixels.append(ps)
+            color = palette_color(self.opt.waterfall_palette, self.vmin + istep * step_value, self.vmin, self.vmax)
+            surface = pg.Surface(self.pixel_size)
+            surface.fill(color)
+            self.pixels.append(surface)
 
     def set_range(self, vmin, vmax):
         """ define a new data range for palette calculation going forward.
             input: vmin, vmax
         """
-        self.vmin = vmin
-        self.vmax = vmax
+        self.vmin, self.vmax = vmin, vmax
         self.initialize_palette()
 
     def reset_range(self):
         """ reset palette data range to original settings.
         """
-        self.vmin = self.vmin_rst
-        self.vmax = self.vmax_rst
+        self.vmin, self.vmax = self.vmin_rst, self.vmax_rst
         self.initialize_palette()
         return self.vmin, self.vmax
 
     def calculate(self, datalist, nsum, surface):   # (datalist is np.array)
-        if self.firstcalc:                          # First time through,
-            self.datasize = len(datalist)           # pick up dimension of datalist
-            self.wfacc = np.zeros(self.datasize)    # and establish accumulator
-            self.dx = float(surface.get_width()) / self.datasize # x spacing of wf cells
-            # Note: self.dx must be >= 1
-            self.wfcount = 0
+        if self.firstcalc:
+            self.datasize = len(datalist)
+            self.wfacc = np.zeros(self.datasize)
+            self.dx = surface.get_width() / self.datasize
             self.firstcalc = False
-        self.wfcount += 1
-        self.wfacc += datalist              # Accumulate data
-        if self.wfcount % nsum != 0:        # Don't plot wf data until enough spectra accumulated
-            return
-        else:
-            surface.blit(surface, (0, self.pixel_size[1]))  # push old wf down one row
-            for ix in range(self.datasize):
-                v = datalist[ix] #self.wfacc[ix] / nsum #datalist[ix]        # dB units
-                if np.isfinite(v): #af mono
-                    vi = int( self.nsteps * (v-self.vmin) / (self.vmax-self.vmin) )
-                    vi = max(0, min(vi, self.nsteps-1) )
-                    px_surf = self.pixels[vi]
-                    x = int(ix * self.dx)
-                    surface.blit(px_surf, (x, 0))
-            self.wfcount = 0                        # Initialize counter
-            self.wfacc.fill(0)                      #   and accumulator
+
+        self.wfacc += datalist
+        self.wfcount = (self.wfcount + 1) if hasattr(self, 'wfcount') else 1
+
+        if self.wfcount % nsum == 0:
+            surface.scroll(dy=int(self.pixel_size[1]))
+            normalized_data = np.clip((self.wfacc / nsum - self.vmin) / (self.vmax - self.vmin), 0, 1) * (self.nsteps - 1)
+            for ix, val in enumerate(normalized_data.astype(int)):
+                surface.blit(self.pixels[val], (int(ix * self.dx), 0))
+
+            self.wfcount = 0
+            self.wfacc.fill(0)
