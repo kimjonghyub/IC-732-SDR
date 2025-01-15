54e07438502fb2efe005d4f1856bf2a76e85e412
kimjonghyub <ds3dnd@gmail.com>
2025-01-15 09:48:07 +0000

    Add files via upload

--- tcp_client9.py
+++ tcp_client9.py
@@ -30,12 +30,12 @@ CMD_SET_GAIN_MODE = 0x03
 CMD_SET_OFFSET_TUNING = 0x0A
 FRAME_SIZE = 512
 BUFFER_SIZE = FRAME_SIZE * 6
-FREQ_RANGE = 240
+FREQ_RANGE = 256
 
 SERVER_IP = "222.117.38.98"
 SERVER_PORT = 1234
 FREQ = 69011000
-SAMPLE_RATE = 1920000
+SAMPLE_RATE = 1024000
 GAIN = 405
 FREQ_CORRECTION = 0
 AGC_MODE = 1
@@ -46,13 +46,38 @@ WIDTH, HEIGHT = 400, 800
 BUTTON_WIDTH, BUTTON_HEIGHT = 100, 35
 FPS = 15
 BUTTON_AREA_HEIGHT = 150
+MARGIN = 50
 FFT_GRAPH_HEIGHT = (HEIGHT - BUTTON_AREA_HEIGHT) // 2
 WATERFALL_HEIGHT = HEIGHT - BUTTON_AREA_HEIGHT - FFT_GRAPH_HEIGHT
+MIN_DB = -50
+MAX_DB = 120
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RTL-TCP IQ Data Visualization")
 clock = pygame.time.Clock()
+
+data_queue = asyncio.Queue(maxsize=2)
+
+waterfall_buffer = np.zeros((WATERFALL_HEIGHT, WIDTH-MARGIN), dtype=np.uint8)
+waterfall_surface = pygame.Surface((WIDTH-MARGIN, WATERFALL_HEIGHT))
+COLORMAP = np.zeros((256, 3), dtype=np.uint8)
+for i in range(256):
+    if i < 128: 
+        r = 0
+        g = int(i * 2)       
+        b = int(255 - i * 2) 
+    else: 
+        r = int((i - 128) * 2) 
+        g = int(255 - (i - 128) * 2) 
+        b = 0
+
+    COLORMAP[i] = [r, g, b]
+
+is_connected = False
+reader = None
+writer = None
+tasks = []  
 
 ctl_box = pygame.Surface(((WIDTH), BUTTON_AREA_HEIGHT) )
 ctl_box.fill(BLACK)
@@ -63,6 +88,7 @@ button_color = (RED)  # Initial color: red
 button_text = "Connect"
 text_color = (BLACK)
 font = pygame.font.SysFont(None, 24)
+font_tick = pygame.font.SysFont(None, 20)
 
 def text_objects(text, text_font, color):
     textSurface = font.render(text, 1, color)
@@ -86,35 +112,11 @@ def button(msg,x,y,w,h,ic,ac,tc,hc,action=None):
     textRect.center = ( (int(x)+(int(w/2))), (int(y)+(int(h/2))) )
     screen.blit(textSurf, textRect)
 
-waterfall_buffer = np.zeros((WATERFALL_HEIGHT, WIDTH), dtype=np.uint8)
-waterfall_surface = pygame.Surface((WIDTH, WATERFALL_HEIGHT))
-COLORMAP = np.zeros((256, 3), dtype=np.uint8)
-
-for i in range(256):
-    if i < 128: 
-        r = 0
-        g = int(i * 2)       
-        b = int(255 - i * 2) 
-    else: 
-        r = int((i - 128) * 2) 
-        g = int(255 - (i - 128) * 2) 
-        b = 0
-
-    COLORMAP[i] = [r, g, b]
-
-
-is_connected = False
-reader = None
-writer = None
-tasks = []  
-
 def upsample_signal(data, factor):
-
     x = np.arange(len(data))
     x_up = np.linspace(0, len(data) - 1, len(data) * factor)
     spline = make_interp_spline(x, data)
     return spline(x_up)
-
 
 def process_iq_data(iq_data):
     iq_data = np.frombuffer(iq_data, dtype=np.uint8).astype(np.float32)
@@ -134,27 +136,78 @@ async def compute_fft(complex_signal, sample_rate):
 def update_waterfall(buffer, fft_row):
     buffer[1:] = buffer[:-1]
     buffer[0] = np.interp(
-        np.linspace(0, len(fft_row) - 1, WIDTH),
+        np.linspace(0, len(fft_row) - 1, WIDTH-MARGIN),
         np.arange(len(fft_row)),
         fft_row
     ).astype(np.uint8)
 
 def draw_waterfall(surface, buffer):
-    rgb_buffer = COLORMAP[buffer].reshape((WATERFALL_HEIGHT, WIDTH, 3))
+    rgb_buffer = COLORMAP[buffer].reshape((WATERFALL_HEIGHT, WIDTH-MARGIN, 3))
     pygame.surfarray.blit_array(surface, np.transpose(rgb_buffer, (1, 0, 2)))
-    screen.blit(surface, (0, HEIGHT - WATERFALL_HEIGHT))
+    screen.blit(surface, (MARGIN/2, HEIGHT - WATERFALL_HEIGHT))
     
 def draw_fft_graph(screen, freqs, magnitudes):
-    mid_x = WIDTH // 2
-    x_coords = mid_x + (freqs / (SAMPLE_RATE / 2)) * mid_x
-    y_coords = BUTTON_AREA_HEIGHT + FFT_GRAPH_HEIGHT - magnitudes
+    # Define reduced width and height for the graph
+    graph_width = WIDTH - MARGIN
+    graph_height = FFT_GRAPH_HEIGHT - MARGIN
+
+    # Define the position of the graph
+    graph_x = MARGIN/2
+    graph_y = BUTTON_AREA_HEIGHT + MARGIN/2
+    
+    # Clear the FFT graph area
+    pygame.draw.rect(screen, GRAY, (graph_x, graph_y, graph_width, graph_height))  
+    
+    mid_x = graph_x + graph_width // 2
+    x_coords = mid_x + (freqs / (SAMPLE_RATE / 2)) * (graph_width // 2)
+    """
+    y_coords = graph_y + graph_height - 10 - (
+        magnitudes / np.max(magnitudes) * (graph_height - 20)
+    )
+    """
+    y_coords = graph_y + (
+        graph_height * (1 - (magnitudes) / (graph_height))
+    )
+    y_coords = np.clip(y_coords, graph_y, graph_y + graph_height)
     points = np.column_stack((x_coords, y_coords)).astype(int)
-    pygame.draw.rect(screen, (BLACK), (0, BUTTON_AREA_HEIGHT, WIDTH, FFT_GRAPH_HEIGHT))  
+    # Create polygon points to fill below the graph
+    polygon_points = [(graph_x, graph_y + graph_height)] + points.tolist() + [
+        (graph_x + graph_width, graph_y + graph_height)
+    ]
+
+    # Fill the area below the FFT graph with a color
+    pygame.draw.polygon(screen, BLUE_GRAY, polygon_points)
     pygame.draw.lines(screen, (WHITE), False, points.tolist(), 1)
     
-          
-data_queue = asyncio.Queue(maxsize=1)
-
+    # Draw x-axis
+    pygame.draw.line(
+        screen, GRAY, 
+        (graph_x, graph_y + graph_height - 1), 
+        (graph_x + graph_width, graph_y + graph_height - 1), 
+        1
+    )
+    # Adjust the frequency range based on freq_range
+    max_freq = SAMPLE_RATE / 2 / (1024 // FREQ_RANGE)  # Adjusted maximum frequency
+    num_ticks = 4  # Number of ticks
+    tick_spacing = graph_width // num_ticks  # Pixel spacing between ticks
+    
+    for i in range(num_ticks + 1):
+        x_pos = i * tick_spacing
+        freq = int((i - num_ticks // 2) * (max_freq / num_ticks) / 1e3)  # Frequency in kHz
+        tick_label = f"{freq}kHz"
+        
+        # Draw tick line
+        pygame.draw.line(
+            screen, GRAY, 
+            (x_pos+graph_x, graph_y + graph_height - 5), 
+            (x_pos+graph_x, graph_y + graph_height + 5), 
+            1
+        )
+        
+        # Render tick label
+        label = font_tick.render(tick_label, True, WHITE)
+        label_rect = label.get_rect(center=(x_pos+graph_x, graph_y + graph_height + 15))
+        screen.blit(label, label_rect)
 
 async def set_freq_range(freq_range):
     global FREQ_RANGE, tasks
@@ -162,7 +215,11 @@ async def set_freq_range(freq_range):
     if writer:
         try:
             await send_command(writer, CMD_SET_SAMPLERATE, SAMPLE_RATE)
+            #await send_command(writer, CMD_SET_FREQ_CORRECTION, FREQ_CORRECTION)
+            #await send_command(writer, CMD_SET_FREQ, FREQ)
             await send_command(writer, CMD_SET_AGC_MODE, AGC_MODE)
+            await send_command(writer, CMD_SET_DIRECT_SAMPLING, DIRECT_SAMPLING)
+            await send_command(writer, CMD_SET_GAIN_MODE, GAIN_MODE)
             
             #print(f"set {SAMPLE_RATE}")
         except Exception as e:
@@ -171,13 +228,13 @@ async def set_freq_range(freq_range):
 
 def process_signal(complex_signal, freq_range):
     global FPS, BUFFER_SIZE
-    if freq_range == 240:
+    if freq_range == 256:
         complex_signal = complex_signal[::4]  
          
-    elif freq_range == 480:
+    elif freq_range == 512:
         complex_signal = complex_signal[::2]  
     
-    elif freq_range == 960:
+    elif freq_range == 1024:
         complex_signal = complex_signal[::1]  
          
         
@@ -228,8 +285,6 @@ async def handle_connection():
     except Exception as e:
         print(f"Connection failed: {e}")
         is_connected = False
-
-
         
 async def close_connection():
     global reader, writer, is_connected, data_queue
@@ -279,11 +334,9 @@ def draw_button():
                        button_rect.y + (BUTTON_HEIGHT - text.get_height()) // 2))
     """
     button("connect",(WIDTH-390),10,100,35,RED,SCREEN,GRAY,WHITE)
-    button("240Khz",(WIDTH-280),10,80,35,SCREEN,SCREEN,GRAY,WHITE,lambda: set_freq_range(240))
-    button("480Khz",(WIDTH-190),10,80,35,SCREEN,SCREEN,GRAY,WHITE,lambda: set_freq_range(480))
-    button("960Khz",(WIDTH-100),10,80,35,SCREEN,SCREEN,GRAY,WHITE,lambda: set_freq_range(960))
-
-
+    button("256Khz",(WIDTH-280),10,80,35,SCREEN,SCREEN,GRAY,WHITE,lambda: set_freq_range(256))
+    button("512Khz",(WIDTH-190),10,80,35,SCREEN,SCREEN,GRAY,WHITE,lambda: set_freq_range(512))
+    button("1024Khz",(WIDTH-100),10,80,35,SCREEN,SCREEN,GRAY,WHITE,lambda: set_freq_range(1024))
 
 async def main():
     global button_color, button_text, is_connected
@@ -312,11 +365,12 @@ async def main():
                 complex_signal = process_signal(process_iq_data(iq_data), FREQ_RANGE)
                 freqs, magnitudes = await compute_fft(complex_signal, SAMPLE_RATE)
                 scaled_magnitudes = np.interp(
-                    magnitudes, [-50, 100], [0, 255]
+                    magnitudes, [MIN_DB, MAX_DB], [0, 255]
                 ).astype(np.uint8)
                 update_waterfall(waterfall_buffer, scaled_magnitudes)
                 draw_waterfall(waterfall_surface, waterfall_buffer)
                 draw_fft_graph(screen, freqs, scaled_magnitudes)
+                #print(f"Magnitudes range: min={magnitudes.min()}, max={magnitudes.max()}")
                 #print(f"Queue size: {data_queue.qsize()}/{data_queue.maxsize}")
             except asyncio.QueueEmpty:
                 pass
